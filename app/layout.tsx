4aae58b898bc57d9a003f17b53a845df3cbe0f0f
SaiRakindi <sai.rakindi3@gmail.com>
2023-12-15 15:49:30 +0530

    feat: changed metadata title

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import "./globals.css";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Anime Vault",
+  title: "Anime Vault - Sai Rakindi",
   description: "Your favorite anime, all in one place.",
 };
 
@@ -19,14 +19,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={dmSans.className}>
-        <main className="max-w-7xl mx-auto bg-[#0F1117]">
-          <Hero />
-          {children}
-          <Footer />
-        </main>
-      </body>
-    </html>
+    <>
+      <html lang="en">
+        <body className={dmSans.className}>
+          <main className="max-w-7xl mx-auto bg-[#0F1117]">
+            <Hero />
+            {children}
+            <Footer />
+          </main>
+        </body>
+      </html>
+    </>
   );
 }
