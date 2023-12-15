eb8b4aa0517becad285fb47c3a21c07ffcbec44e
SaiRakindi <sai.rakindi3@gmail.com>
2023-12-15 13:27:29 +0530

    feat: infinite scrolling based on pages

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,18 +1,46 @@
+"use client";
+
+import { fetchAnime } from "@/app/action";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+
+import { useInView } from "react-intersection-observer";
+import AnimeCard, { AnimeProp } from "./AnimeCard";
+
+let page = 2;
 
 const LoadMore = () => {
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<AnimeProp[]>([]);
+
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(page).then((res) => {
+        setData([...data, ...res]);
+        page++;
+      });
+    }
+  }, [inView, data]);
+
   return (
-    <section className="flex justify-center items-center w-full">
-      <div>
-        <Image
-          src="./spinner.svg"
-          alt="spinner"
-          width={56}
-          height={56}
-          className="object-container"
-        />
-      </div>
-    </section>
+    <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((item: AnimeProp, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
+      <section className="flex justify-center items-center w-full">
+        <div ref={ref}>
+          <Image
+            src="./spinner.svg"
+            alt="spinner"
+            width={56}
+            height={56}
+            className="object-container"
+          />
+        </div>
+      </section>
+    </>
   );
 };
 
